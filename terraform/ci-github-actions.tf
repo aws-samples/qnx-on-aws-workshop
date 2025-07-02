a1fe085fe762c5a4b43cd8d96e81d401d290a2d2
Takechika Kunimasa <kunimasa@amazon.com>
2025-07-02 17:11:16 +0900

    Update

--- terraform/ci-github-actions.tf
+++ terraform/ci-github-actions.tf
@@ -15,6 +15,9 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
     "sts.amazonaws.com",
   ]
 
+  # GitHub Actions OIDC provider thumbprint list
+  # These thumbprints are used to verify the authenticity of the GitHub Actions OIDC provider
+  # Reference: https://github.blog/changelog/2023-06-27-github-actions-update-on-oidc-integration-with-aws/
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
